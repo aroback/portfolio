1539b62bd729726d5a700437aabdd971fec83e98
Andrew Roback <andrew.roback@gmail.com>
2011-01-04 23:53:01 -0600

    list is now styled better--could use some work

--- java/test.js
+++ java/test.js
@@ -1,8 +1,8 @@
-<script type="text/javascript">
+ //from http://webdesign.about.com/od/dhtml/a/aa101507.htm
+ 
  function unhide(divID) {
  var item = document.getElementById(divID);
  if (item) {
  item.className=(item.className=='hidden')?'unhidden':'hidden';
  }
  }
- </script>
