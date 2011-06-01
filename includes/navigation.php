bbae0b83edc1b0f7b0bb05ac99389ee07a7456b2
arobck <andrew.roback@gmail.com>
2011-05-31 23:46:32 -0500

    fixed a variety of validation errors

--- includes/navigation.php
+++ includes/navigation.php
@@ -4,6 +4,6 @@
 		<li><a href="../cv/">Curriculum Vitae</a></li>
 		<li><a href="../design/">Design Projects</a></li>
 		<li><a href="../papers/">Select Papers</a></li>
-		<li><a href="http://blog.andrewroback.com">Blog</a>
+		<li><a href="http://blog.andrewroback.com">Blog</a></li>
 	</ul>
 </div>
